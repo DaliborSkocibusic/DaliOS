8120c1240a3a8e56aa3950a2325c5e117d1c16e0
DaliborSkocibusic <skody001@mymail.unisa.edu.au>
2023-09-18 22:02:23 +0900

    Print Hello World - L5

--- boot.asm
+++ boot.asm
@@ -3,13 +3,31 @@ ORG 0x7c00  ; Specify this as the start point of the asm program
             ; The origin should be 0 and we should jump ideally
 BITS 16     ; Tells the assembler to only assemble 16 bit code
 
-start:      ; This is a label, this is where our program starts
-    mov ah, 0eh ; This is real mode (16 bit programming)
-    mov al, 'A' ; Move A into the A register
-    mov bx, 0
-    int 0x10    ; Call the interupt that will output the char to the screen
 
+start:      ; This is a label, this is where our program starts
+    mov si, message
+    call print
     jmp $       ; Jump to the same spot. Inf loop
+
+print:
+    mov bx, 0
+
+.loop: 
+    lodsb               ; will load charcher si is pointing to
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+
+.done:                  ; NASM can set labels for only the label above. idk what that means
+    ret
+
+print_char:
+    mov ah, 0eh ; This is real mode (16 bit programming)
+    int 0x10    ; Call the interupt that will output the char to the screen
+    ret
+
+message: db 'Dalibors Amazing Operating System!', 0 ; Set a lable message as a double byte 2x8 with 0 null terminator
 
 times 510- ($ - $$) db 0 ; This fills the first 510 bytes with 0 or the content to make a sector
 ; Line above will fill the 510 bytes with data if nothing and pad with 0
