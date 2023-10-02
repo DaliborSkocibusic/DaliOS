93f3fc81b163062e4c3f2ae5e293efe707a5d04e
DaliborSkocibusic <skody001@mymail.unisa.edu.au>
2023-10-02 14:52:30 +0900

    Now we are in protected mode. L17

--- boot.asm
+++ boot.asm
@@ -1,185 +1,117 @@
 ; The BIOS will load the program into address Ox7C00
-ORG 0         ; Specify this as the start point of the asm program. 0 Now
+ORG 0x7c00         ; Specify this as the start point of the asm program. 0 Now
             ; The origin should be 0 and we should jump ideally
 BITS 16     ; Tells the assembler to only assemble 16 bit code
             ; Dont forget this starts in real mode
             ; That means only 1MB of RAM, no security max 16 bit address space
             ; Real mode has to be 16 bits
 
-;; From 14. Disk Access And How It Works
-;; LBA Logical block address is how we read from disks. LBA0 First sector, LBA1 Second Sector
-;; Lets us read large files. Sector typically 512b unles CDRom
-;; To calc say we want to read the byte at 58376
-;; LBA = 58376 / 512 = 114 ; Read that LBA so we can load those 512 bytes into memory / buffer
-;; Need to know offset for that particular spot
-;; 512b is loaded into buffer, need to figure offset to load it
-;; Offset = 58376 % 512 = 8
-;; in 16 bit real mode, int 13h does disk ops
-;; in 32 bit mode we need our own driver
-
-;; 15. Reading from the hard disk
-;; Lets make a makefile ; sudo apt install make
-;; to run make file type : make
-
-;; Also need bless hex editor
-;; sudo apt install bless
-;; after making we want to do bless ./boot.bin
-;; Second sector is not 512 bytes. Guessing dd if=message pushed evertyhing of our messager into the text file
-;; now we need to set it to 512 bytes so it works
-
-
-;;;;;;;;;;;;;;;;;;;
-; Code below is to give space for the code that is overwritten when booting from usb
-; Data in that section must not be important asit will be overwritten by BIOS
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gtd_data - gdt_start
 _start: ; leaves 3 bytes for BIOS when loading from USB
     jmp short start
     nop
 
 times 33 db 0 ;; Fills in 33 null bytes after short start
 
-
 start:      ; This is a label, this is where our program starts
+    jmp 0:step2 ; Sets code segment to 7c0
 
-    jmp 0x7c0:step2 ; Sets code segment to 7c0
-
-
-
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Section below modifies the code so that all the registers are set manually
-; As opposed to expecing BIOS to do it for us. 
-
-
-;; Interrupt handler
-;; This is us changing the interrupt vector table
-;; Our interrupts will start at 0 aka the Orgin
-
-
-; Commented Lecture 15
-; handle_zero: ; This will be called when someone does int 0
-;     mov ah, 0eh
-;     mov al, 'A'
-;     mov bx, 0x00
-;     int 0x10
-;     iret
-
-; ;; To assemble nasm -f bin ./boot.asm -o ./boot.bin
 ; ;; To run qemu-system-x86_64 -hda ./boot.bin 
+;; to run on usb stick
+;; go to bootloader dir
+;; sudo fdisk -l ; shows all file systems
+;; usb likely /dev/sdb
+;; get dd 
+;; sudo dd if=./boot.bin of=./dev/sdb
 
-; ;; From 13. Implementing our own interrupts in real mode
-; handle_one:
-;     mov ah, 0eh
-;     mov al, 'V'
-;     mov bx, 0x00
-;     int 0x10
-;     iret
-;; From 13. Implementing our own interrupts in real mode
-; Commented Lecture 15
+;; need to make sure this is working with gdb
+;; sudo apt install gdb
+;; crank with $gdb
+;;            (gdb) target remote | qemu-system-x86_64 -hda ./boot.bin -S -gdb stdio
+;; step in with c
+;; Ctrl + c to go to (gdb) you can see what line you are on
+;; layout asm shows th easm
+;; info registers shows we are in prot mode by cs = 8
 
 
+;; We are now in protected mode
+;; We cant call interrupts or read from disk
+;; interrupts will make bad things happen. idk what yet..
+;; we need our own drivers
 
 step2:
     cli     ; Clear interrupt flags disables hardware interrupts
-    mov ax, 0x7c0        ; Necessary when setting registers
+    mov ax, 0x00          ; Necessary when setting registers
     mov ds, ax            ; Put ax into ds
     mov es, ax            ; put ax into es
-
-    mov ax, 0x00            ; set stack segment to 00
     mov ss, ax                    
     mov sp, 0x7c00          ; set the stack pointer to the highest memry location
     sti     ; Set interupts / Enables interrupts
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    ;; Commented In lecture 15
-    ;; This is reading from hdd using CHS method
-    ;; This is how  you write to disks using the kernal
-    ;; CHS apparently the ohter one doesnt work. ok. 
+;; This is us going into protected mode. I think
+.load_protected:
+    cli
+    lgdt[gdt_descriptor] ;; Load the descriptor table at gdt_descriptor
+    mov eax, cr0 ; 
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32 ; COde seg is 0x8
 
-    mov ah, 2 ; Read sector command
-    mov al, 1 ; We are reading one sector
-    mov ch, 0 ; Cylinder number is 0
-    mov cl, 2 ; Starts at 1 for CHS
-    mov dh, 0 ; Head number
+; Global Descriptor Table
+; https://wiki.osdev.org/Global_Descriptor_Table
+; It tells the cpu about memory segments
 
-    ; set bx to the buffer
-    mov bx, buffer
-    int 0x13
-    ; dl loaded to current drive as default
+gdt_start:
 
-    jc error ; if the carry flag is set, i.e. did not read properly will jump to error. 
 
-    mov si, buffer
-    call print
+gdt_null:   ; Need a null segment
+    dd 0x0 ; 32bit of 0's or 4 bytes
+    dd 0x0
+
+; offset 0x8
+gdt_code:     ; CS should point to this
+    dw 0xffff ; Segment limit of first 0 - 15 bits
+    dw 0      ; Base 0 -15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0      ; Base 24-31 bits
+
+; Ofset 0x10
+gtd_data:     ; Should be linked to DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit of first 0 - 15 bits
+    dw 0      ; Base 0 -15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0      ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ;; This is the size
+    dd gdt_start               ;; This is the offset
+
+[BITS 32] ;; Tesl the asm this is 32 bit code
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    ;; Setting all the registers to start from the data seg in memeory
+    mov ebp, 0x00200000 ;; ebp now were in 32 bit i.e. 4 byte or 00 20 00 00
+    mov esp, ebp
     jmp $
-
-error:
-    mov si, error_message
-    call print    
-    jmp $       ; Jump to the same spot. Inf loop
-
-
-    ;; Commented out Lecture 15
-        ; ;;; This is us doing something regaring calling it in the irt...
-        ; mov word[ss:0x00], handle_zero ; This is specifying that we are going to be using ss as the stack pointer reg 
-        ; ;;; Might not actually be the stack pointer, whatever the pointer is being used when we run code
-        ; ;; if we dont use ss it will use ds which points currently to 0x7c0...
-        ; mov word[ss:0x02], 0x7c0 ; 
-
-        ; mov word[ss:0x04], handle_one ; Moving handle one into the interrupt vector table
-        ; mov word[ss:0x06], 0x7c0 ; this is interrupt 1
-        ; ;; int 0 ; Now we will call the interrupt. Obs dont want this here this is just for test. 
-        ; ;; From 13. Implementing our own interrupts in real mode
-        ; ;; When int 0 i scalled A is pritnerd to screen. 
-
-        ; int 1 ; We are calling interrupt 1. This will print v for now
-
-        ; ;; From 13. Implementing our own interrupts in real mode
-        ; ;mov ax, 0x00
-        ; ;div ax ;; This makes a div by 0 error i.e. the interrupt is called. 
-        ; mov si, message
-        ; call print
-
-    ;; Commented out Lecture 15
-
-
-
-print:
-    mov bx, 0
-
-.loop: 
-    lodsb               ; will load charcher si is pointing to
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-
-.done:                  ; NASM can set labels for only the label above. idk what that means
-    ret
-
-print_char:
-    mov ah, 0eh ; This is real mode (16 bit programming)
-    int 0x10    ; Call the interupt that will output the char to the screen
-    ret
-
-    ; message: db 'Dalibors Amazing Operating System!', 0 ; Set a lable message as a double byte 2x8 with 0 null terminator
-error_message: db 'Failed to load sector', 0 ; 0 is null term character
 
 times 510- ($ - $$) db 0 ; This fills the first 510 bytes with 0 or the content to make a sector
 ; Line above will fill the 510 bytes with data if nothing and pad with 0
 dw 0xAA55 ; Need 55AA in the last two bytes so BIOS knows to load this sector
             ; Big endian. 
 
-
-
-;; Need a label for the buffer we will load from hdd from
-;; Lecture 15
-
-buffer:
-
-
 ;; One byte is two hex digits
 ;; i.e. 0x00 to 0xFF is a single byte
 ;; 512b is 0x200
+;; Commit 15 is good. 
